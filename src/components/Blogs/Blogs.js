2905aac638cd5de9fa9f075507449cb42b64ed1b
Fahim <fahim.madani@gmail.com>
2022-04-04 19:53:09 +0600

    blogs re-edit

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -7,7 +7,7 @@ const Blogs = () => {
             <h3>What is a Semantic Tag?</h3>
             <p>Semantic tag aims to make our code more <b>meaningful</b> which makes our codes more <b>descriptive</b> and it is easier to optimize with SEO. Semantic Tags were introduced in HTML5. Examples of semantic tags are header, main, footer etc</p>
             <h3>What is Context API?</h3>
-            <p>Context API enables sharing of data between components without passing down props manually. Through Context API we can share strings, objects, functions to any component, be it father components or child components. Wi</p>
+            <p>Context API enables sharing of data between components without passing down props manually which is time exhausting and frustrating for building large application but through Context API we can share strings, objects, functions to any components between father component and child component without prop drilling. </p>
         </div>
     );
 };
